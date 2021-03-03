aa99dc897a12c5a664a85fe39cebbf31caf3e02b
faranewdtech <faran@ewdtech.com>
2021-03-03 13:04:00 +0500

    update data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import array from './components/arrays';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table} from "react-bootstrap";
+
 
 function App() {
+const [listitem,setitem]=useState(array);
+function handleRemove(id){
+  const newList = listitem.filter((data) => data.id !== id);
+ 
+    setitem(newList);
+  console.log(id)
+
+}
+const tabledata = listitem.map(data=> {
+  return <>
+  
+  <tr>
+<td>{data.Name}</td>
+<td>{data.City}</td>
+<td>{data.Age}</td>
+<td><button onClick={()=>handleRemove(data.id)}>Delete</button></td>
+</tr>
+  </>
+})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     {/* {listitem.map((data)=>(
+       <div key={data.id} >{data.Name}<br/>{data.City}<br/>{data.Age}
+       </div>
+      
+     ))} */}
+  <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+ 
+  {tabledata}
+  </tbody>
+</Table>
     </div>
   );
 }
---
+++ src/components/arrays.js
@@ -0,0 +1,23 @@
+const array=[
+    {
+        id:"1",
+    Name:"Rashid",
+    City:"Faisalabad",
+    Age:"23 years"
+},
+{
+    id:"2",
+    Name:"Nadeem",
+    City:"islamabad",
+    Age:"29 years"
+},
+{
+    id:"3",
+    Name:"Arsal",
+    City:"Lahore",
+    Age:"24 years"
+}
+
+]
+export default array;
+
